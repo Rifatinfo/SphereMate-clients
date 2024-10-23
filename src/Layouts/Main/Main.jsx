1c4e7921e43d844cb092dd7b4e24c704370af3d9
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-10-23 20:05:45 +0600

    first commit

---
+++ src/Layouts/Main/Main.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../../Components/Navbar/Navbar";
+
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Main;
+
+
+
+
